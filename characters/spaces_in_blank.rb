e28a8acfe468635b7ed95074af6af38f2c6e17a0
Adenilson Santos <adenilsonblog@gmail.com>
2019-05-12 09:35:40 -0300

    Break line for best experience

--- characters/spaces_in_blank.rb
+++ characters/spaces_in_blank.rb
@@ -1,12 +1,13 @@
 text = 'a   b'
-#option 1
+
+# option 1
 puts text.match(/a   b/)
 puts text.match(/a\s\s\sb/)
 
-#option 2
+# option 2
 puts text.match(/a {3}b/)
 puts text.match(/a\s{3}b/)
 
-#option 3
+# option 3
 puts text.match(/a +b/)
 puts text.match(/a\s+b/)
