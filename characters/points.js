8af47e1e46e2881e706c67820723e85975de48f4
Adenilson Santos <adenilsonblog@gmail.com>
2019-05-12 09:36:01 -0300

    Points test by breakline

---
+++ characters/points.js
@@ -0,0 +1,6 @@
+text = `
+testando frase com
+ponto e quebra
+de linha
+`
+console.log(text.match(/./g))
---
+++ characters/points.rb
@@ -0,0 +1,7 @@
+text = <<EOM
+testando frase com
+ponto e quebra
+de linha
+EOM
+
+puts text.scan(/./)
