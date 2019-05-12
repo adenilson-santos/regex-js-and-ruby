f55c4c026e040375d673b7b7ed19bdeb53994c65
Adenilson Santos <adenilsonblog@gmail.com>
2019-05-12 12:36:45 -0300

    begin with sets

---
+++ sets/begin.js
@@ -0,0 +1,3 @@
+const text = "João nao vai passear de moto e então joão não será mais seu amigo."
+
+console.log(text.match(/jo[aã]o n[aã]o/gi))
---
+++ sets/begin.rb
@@ -0,0 +1,3 @@
+text = "João nao vai passear de moto e então joão não será mais seu amigo."
+
+puts text.scan(/jo[aã]o n[aã]o/i)
